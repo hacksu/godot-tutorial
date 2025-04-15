6874f1cf4934e0ed2ade80dbe98361f770f6f518
Andrew Roddy <andrewroddy7@gmail.com>
2025-04-15 08:27:15 -0400

    0.06.00 C4 - Speedy Walls

--- C4 - Speedy Walls/ball.gd
+++ C4 - Speedy Walls/ball.gd
@@ -5,10 +5,12 @@
 extends CharacterBody2D
 
 var speed = 4
-var multiplier = 1.1
+
 ## NEW # NEW # NEW # NEW # NEW ##
+var multiplier = 1.1
 var game_over = false
 #################################
+
 var collision = move_and_collide(velocity)
 
 func _ready() -> void:
@@ -16,20 +18,26 @@ func _ready() -> void:
 	velocity.y = speed
 	
 func _physics_process(delta: float) -> void:
+	
 	## NEW # NEW # NEW # NEW # NEW ##
 	if game_over:
 		position.x = 1000
 		position.y = 50
 		game_over = false
 	#################################
+	
 	var collision = move_and_collide(velocity)
 	if collision:
 		var collider = collision.get_collider()
 		var collision_normal = collision.get_normal()
 		if (
+			
+			## NEW # NEW # NEW # NEW # NEW ##
 			collider.name == "Left_Wall" or
 			collider.name == "Right_Wall" or
-			collider.name == "Top_Wall" or
+			collider.name == "Top_Wall" or 
+			#################################
+			
 			collider.name == "Cat"
 		):
 			if (collision_normal == Vector2(1, 0) or 
@@ -39,12 +47,16 @@ func _physics_process(delta: float) -> void:
 				collision_normal == Vector2(0, -1)):
 				velocity.y = velocity.y * -1
 		
+		## NEW # NEW # NEW # NEW # NEW ##
 		if collider.name == "Cat":
 			var cat_velocity = collider.velocity.x
 			velocity.x += cat_velocity * 0.01 
+		#################################
+		
 		if (collider.name == "Bottom_Wall"):
 			velocity.x = speed
 			velocity.y = speed
+			
 			## NEW # NEW # NEW # NEW # NEW ##
 			game_over = true
 			#################################
---
+++ C4 - Speedy Walls/cat.gd
@@ -0,0 +1,28 @@
+# cat.gd
+# Written by Andrew Roddy 
+# 4/15/2025
+
+# It's kind of like Javascript where you have to tell the program 
+extends CharacterBody2D
+
+const SPEED = 300.0 # Player speed
+
+# This runs every delta frame meaning every frame multiplied by how bad your computer is
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# Godot has really convenient button mapping stuff so we can edit what
+	# "ui_left" and "ui_right" do later but we wont :)
+	var direction := Input.get_axis("ui_left", "ui_right")
+
+	if direction: # This changes our x position to where we are tryna move
+		velocity.x = direction * SPEED
+	else: # If we don't have a direction yet, stand still
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Then this last part just makes sure our velocity.y stays 0
+	# When the ball bounces off of us we don't want to be pushed back
+	velocity.y = 0
+
+	# Then this guy interprets all of that and actually does the moving
+	move_and_slide() 
